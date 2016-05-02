3e0584feb01c9679642cabeab29ce0162aa07352
ingeborg ødegård oftedal <i.o.oftedal@usit.uio.no>
2016-05-02 13:43:52 +0200

    wait step on side effects

--- TusenTac/main/tasks/side-effect/SideEffectTask.swift
+++ TusenTac/main/tasks/side-effect/SideEffectTask.swift
@@ -18,10 +18,10 @@ public var SideEffectTask: ORKOrderedTask {
     instructionStep.title = "Registrering av bivirkninger".localized
     instructionStep.text = "På den neste siden kan du registrere eventuelle bivirkninger du har opplevd.".localized
     steps += [instructionStep]*/
- 
+    
     
     let textChoices = [ORKTextChoice(text: "Skjelving", value: "Skjelving"),
-                       ORKTextChoice(text: "Diarè", value: "Diarè"),
+                       ORKTextChoice(text: "Diaré", value: "Diaré"),
                        ORKTextChoice(text: "Kvalme", value: "Kvalme"),
                        ORKTextChoice(text: "Hodepine", value: "Hodepine"),
                        ORKTextChoice(text: "Annet", value: "Annet"),
@@ -31,7 +31,13 @@ public var SideEffectTask: ORKOrderedTask {
     
     let newSideEffectStep = ORKQuestionStep(identifier: Identifier.NewSideEffectStep.rawValue, title: "Registrering av bivirkninger".localized, answer: newSideEffectAnswer);
     
-    newSideEffectStep.text = "Velg en eller flere nye/forverring av bivirkninger"
+    //let italics = "nye/forverring"
+    let quesText = "Velg en eller flere nye/forverring nye/forverring av bivirkninger"
+    
+
+    
+    newSideEffectStep.text = quesText
+    
     
     steps+=[newSideEffectStep]
     
@@ -44,10 +50,17 @@ public var SideEffectTask: ORKOrderedTask {
     steps+=[oldSideEffectStep]
     
     // SUMMARY STEP
-    let summaryStep = ORKCompletionStep(identifier: Identifier.SummaryStep.rawValue)
+    /*let summaryStep = ORKCompletionStep(identifier: Identifier.SummaryStep.rawValue)
     summaryStep.title = "Levert!".localized
     summaryStep.text = "Dine bivirkninger har blitt registrert.".localized
-    steps += [summaryStep]
+    steps += [summaryStep] */
+    
+    
+    let waitStepIndeterminate = ORKWaitStep(identifier: Identifier.WaitCompletionStep.rawValue)
+    waitStepIndeterminate.title = "Ferdig"
+    waitStepIndeterminate.text = "Laster opp..."
+    waitStepIndeterminate.indicatorType = ORKProgressIndicatorType.Indeterminate
+    steps += [waitStepIndeterminate]
     
     return ORKOrderedTask(identifier: Identifier.SideEffectTask.rawValue, steps: steps)
     
