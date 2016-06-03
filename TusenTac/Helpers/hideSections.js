f5a089d68273213f71f5179602198bf8164c476f
pmitche <paulpmit@gmail.com>
2016-06-03 12:46:06 +0200

    Removed unused js file

--- TusenTac/Helpers/hideSections.js
+++
@@ -1,3 +0,0 @@
-var styleTag = document.createElement("style");
-styleTag.textContent = 'div#app-head-wrapper, #app-footer-wrapper, #privacy-policy-footer, #contact-wrapper {display:none !important; }';
-document.documentElement.appendChild(styleTag);
