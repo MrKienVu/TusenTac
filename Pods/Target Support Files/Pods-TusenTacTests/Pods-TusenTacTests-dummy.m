3e09f1e3a37add537fe16049643735dcc92c4a7f
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2016-06-22 13:31:16 +0200

    Byt ut CocoaPods med Carthage

--- Pods/Target Support Files/Pods-TusenTacTests/Pods-TusenTacTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TusenTacTests : NSObject
-@end
-@implementation PodsDummy_Pods_TusenTacTests
-@end
--- Pods/Target Support Files/Pods-TusenTacUITests/Pods-TusenTacUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TusenTacUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TusenTacUITests
-@end
